b522eaca778e2933e309f6f647f53bab8efd05e4
Kelvin888 <35603292+kelvin888@users.noreply.github.com>
2023-09-19 13:44:52 +0100

    chore: create admin layout (#2)

---
+++ src/app/sales/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminLayout from '../components/AdminLayout/AdminLayout'
+
+const Sales = () => {
+    return (
+        <AdminLayout>
+            sales page
+        </AdminLayout>
+    )
+}
+
+export default Sales
